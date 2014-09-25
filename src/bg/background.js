e02d1a0a9994c42ab6439d6a165fb210d7558c2e
Michael Melchione <michaelmelchione@Michaels-MacBook-Pro.local>
2014-09-25 12:03:36 -0400

    Added documentation

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,18 +1,19 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-//example of using a message handler from the inject scripts
+/**
+ * Executes inject.js into page with tooltipster and jQuery
+ * @returns {void}
+ */	
 chrome.tabs.onUpdated.addListener(function() {
-  chrome.tabs.executeScript(null, { file: "src/inject/jquery-2.1.1.min.js" }, function() {
-    chrome.tabs.executeScript(null, { file: "js/jquery.tooltipster.min.js" });
-    chrome.tabs.executeScript(null, { file: "src/inject/inject.js" });
+  chrome.tabs.executeScript(null, {file: "src/inject/jquery-2.1.1.min.js"}, function() {
+  	chrome.tabs.executeScript(null, {file: "js/jquery.tooltipster.min.js"});
+    chrome.tabs.executeScript(null, {file: "src/inject/inject.js"});
   });   
 });
 
+/**
+ * Message listener - Getter/Setter for extension's Local Storage
+ * @param {JSON}
+ * @returns {JSON}
+ */	
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
 	if(request.method == "getLocalStorage"){
 		sendResponse({"keys": localStorage.getItem("keys")});
