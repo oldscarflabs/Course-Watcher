c128d161ca6b558a5be9114153f7d66dee8942d8
aaronheck <aaronrosenheck@Aarons-MacBook-Pro.local>
2014-10-23 20:51:48 -0400

    Made minor changes

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,7 +5,7 @@ var tooltips = [];
  * Adds the new watch into Local Storage
  * @param {JSON}
  * @returns {void}
- */	
+ */
 function successHandler(data){
 	var localKeys;
 	chrome.extension.sendMessage({method: "getLocalStorage"}, function(response){
@@ -14,14 +14,14 @@ function successHandler(data){
 		if(localKeys == null){
 			localKeys = {
 				"keys": [
-					
+
 				]
 			}
 		}
 		else{
 			localKeys = JSON.parse(localKeys);
 		}
-		
+
 		localKeys['keys'].push({'watch_id': data['watch_id'], 'department': data['department'], 'course': data['course'], 'section': data['section'], 'index': data['index'], 'title':data['title']});
 		chrome.extension.sendMessage({method: "setLocalStorage", data: JSON.stringify(localKeys)}, function(response){});
 	});
@@ -29,22 +29,22 @@ function successHandler(data){
 /**
  * Inserts "WATCH" header with Eye Icon into Rutgers course page
  * @returns {void}
- */	
+ */
 function sectionsHaveLoaded(){
 	var count = 0;
 	$("<span>WATCH</span>").insertAfter(".indexNumberColumnHeader");
 	$("div.sectionHeaders span").css('padding-right', '0px');
 
     $( ".sectionData" ).each(function(){
-		var appendString = $('<div style="display: inline-block" id="snipe" count="' + count + '"><img src="' + chrome.extension.getURL('/icons/eyecon.png') + ' " style="cursor:pointer")></div>');			
+		var appendString = $('<div style="display: inline-block" id="snipe" count="' + count + '"><img src="' + chrome.extension.getURL('/icons/eyecon.png') + ' " style="cursor:pointer")></div>');
 		var email = localStorage.getItem("email");
 
 		appendString.insertAfter($(this).find('.sectionIndexNumber'));
-		
+
 		if(email == null){
 			email = '';
 		}
-		
+
 		var formObject = $("<form id='emailForm'><div style='text-align:center'>Watch This Course</div> <br> Email: <input type='email' id='email' value='" + email + "'></input><button type='button' class='submitSnipe' count='" + count + "'>Watch!</button></form>");
 
 		appendString.tooltipster({
@@ -56,9 +56,9 @@ function sectionsHaveLoaded(){
     	tooltips.push(appendString);
     	count = count + 1;
 	});
-	
+
 	$("#numCoursesFoundDivSpan").attr('id',''); //preps for changing department
-	
+
 	/**
 	 * Click handler for Tooltip
 	 * Posts course data to database via Old Scarf Labs API
@@ -75,12 +75,12 @@ function sectionsHaveLoaded(){
 		var sectionNumber = res[2].replace('section', '');
 		var deptAndCourseNums = courseNumber.split(':');
 		var departmentNum = deptAndCourseNums[1];
-		var courseNum = deptAndCourseNums[2];			
+		var courseNum = deptAndCourseNums[2];
 		var index = snipe.prev().text();
 		var title = snipe.closest('.courseData').siblings('.courseInfo').children('.courseTitleAndCredits').children('.courseTitle').children('.highlighttext').text();
-		
+
 		var postData = {"DepartmentNumber": departmentNum,  "CourseNumber": courseNum,  "Email":email, "SectionNumber": sectionNumber, "Index": index, "Title" : title};
-		
+
 		localStorage.setItem("email",email);
 
 		/**
@@ -91,15 +91,16 @@ function sectionsHaveLoaded(){
 			type: "POST",
 			url: "https://oldscarflabs.me/coursewatcher/addSnipe.php",
 			data: postData,
-			success: function(data){ 
+			success: function(data){
 				successHandler(data);
 			},
 			error: function(data){
 				console.log('POST to Old Scarf Lab API failed with data response:');
 				console.log(data);
+				alert("Something went wrong. Sorry! Please refresh the page and try again.");
 			}
 		});
-		
+
 		var changeTooltip = tooltips[parseInt(number)];
 		changeTooltip.tooltipster('content', 'You are watching this course!');
 	});
